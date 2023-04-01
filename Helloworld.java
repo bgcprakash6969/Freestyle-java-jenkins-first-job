b637db48995aae5a94c237f034e1d20020d2a8ce
bgcprakash6969 <129256406+bgcprakash6969@users.noreply.github.com>
2023-04-01 12:44:30 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,6 @@ public class Helloworld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to the new world and say Hello world");
+		System.out.println("Welcome to the new world and say Hello world!!");
 	}
 }
